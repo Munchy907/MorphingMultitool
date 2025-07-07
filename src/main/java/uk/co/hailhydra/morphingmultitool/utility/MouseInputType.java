d867b4ee832f4ae70d7dce6d9f785185d4c6afe2
MichaelH <Michaelhumphreys2002@gmail.com>
2025-07-07 10:51:31 +0200

    Added MouseInputType which is a constant class for the mouse input types which are stored as ints but class has the name. It cud of been enum

---
+++ src/main/java/uk/co/hailhydra/morphingmultitool/utility/MouseInputType.java
@@ -0,0 +1,9 @@
+package uk.co.hailhydra.morphingmultitool.utility;
+
+import scala.tools.nsc.doc.model.Public;
+
+public class MouseInputType {
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int MIDDLE = 2;
+}
