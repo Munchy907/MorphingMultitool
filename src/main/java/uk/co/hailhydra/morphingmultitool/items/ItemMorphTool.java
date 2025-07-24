f966cabdf22f938914fe225f574cc2c8851f5d9b
MichaelH <Michaelhumphreys2002@gmail.com>
2025-07-24 23:26:43 +0200

    Forgotten unused imports removed. Removed canFit

--- src/main/java/uk/co/hailhydra/morphingmultitool/items/ItemMorphTool.java
+++ src/main/java/uk/co/hailhydra/morphingmultitool/items/ItemMorphTool.java
@@ -1,15 +1,9 @@
 package uk.co.hailhydra.morphingmultitool.items;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.*;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants;
 import uk.co.hailhydra.morphingmultitool.handlers.MorphHandler;
-import uk.co.hailhydra.morphingmultitool.utility.MorphToolResources;
 
 public class ItemMorphTool extends ItemModBase {
     public ItemMorphTool() {
@@ -20,9 +14,5 @@ public class ItemMorphTool extends ItemModBase {
     public void onCreated(ItemStack stack, World worldIn, EntityPlayer playerIn) {
         stack.setTagCompound(MorphHandler.createNBTData(stack));
         super.onCreated(stack, worldIn, playerIn);
-    }
-
-    private boolean canFit(int toolClass){
-        return false;
     }
 }
